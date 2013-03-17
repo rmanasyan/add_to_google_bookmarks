9ca9f31eb3a2b243263f127c363a297ffafb3c24
Roman Manasyan <rmanasyan@gmail.com>
2013-03-17 18:43:17 +0200

    center position popup window

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 // Add Bookmark
 function addBookmark(info, tab) {
+    var w = 560, h = 460;
     chrome.windows.create({
         url: "http://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title),
         type: "popup",
-        height: 540,
-        width: 650
+        width: w,
+        height: h,
+        top: (screen.height/2)-(w/2),
+        left: (screen.width/2)-(h/2)
     });
 }
 
