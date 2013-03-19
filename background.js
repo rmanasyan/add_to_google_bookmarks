da3a63bfb0df8c0335b53f7b6a6e891e7e0b316a
Roman Manasyan <rmanasyan@gmail.com>
2013-03-19 10:54:31 +0200

    fix undefined annotation

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ function addBookmark(info, tab) {
 
     var w = 560, h = 460;
     window.open (
-        "http://www.google.com/bookmarks/mark?op=edit&output=popup&annotation="+info.selectionText+"&bkmk="+encodeURIComponent(tab.url)+"&title="+encodeURIComponent(tab.title),
+        "http://www.google.com/bookmarks/mark?op=edit&output=popup&annotation="+encodeURIComponent(info.selectionText || '')+"&bkmk="+encodeURIComponent(tab.url)+"&title="+encodeURIComponent(tab.title),
         "bkmk_popup",
         "left="+((screen.width/2)-(h/2))+", top="+((screen.height/2)-(w/2))+", height="+h+"px, width="+w+"px, resizable=1, alwaysRaised=1"
     );
