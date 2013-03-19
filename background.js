176881c00bb92de968878b4d68bf3f2e784bfa76
Roman Manasyan <rmanasyan@gmail.com>
2013-03-19 02:20:29 +0200

    fill in annotation (Notes)

--- background.js
+++ background.js
@@ -3,20 +3,12 @@ function addBookmark(info, tab) {
 
     var w = 560, h = 460;
     window.open (
-        "http://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk="+encodeURIComponent(tab.url)+"&title="+encodeURIComponent(tab.title),
+        "http://www.google.com/bookmarks/mark?op=edit&output=popup&annotation="+info.selectionText+"&bkmk="+encodeURIComponent(tab.url)+"&title="+encodeURIComponent(tab.title),
         "bkmk_popup",
         "left="+((screen.width/2)-(h/2))+", top="+((screen.height/2)-(w/2))+", height="+h+"px, width="+w+"px, resizable=1, alwaysRaised=1"
     );
 
-//    var windowId = chrome.windows.create({
-//        url: "http://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title),
-//        type: "popup",
-//        width: w,
-//        height: h,
-//        focused: true,
-//        top: (screen.height/2)-(w/2),
-//        left: (screen.width/2)-(h/2)
-//    });
+    //alert(JSON.stringify(info));
 
 }
 
